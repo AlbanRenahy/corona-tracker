42a63632869ef737dc26bf3f4d25e63c5d49c74f
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-19 14:04:12 +0200

    Countries are fetched

--- src/api/index.js
+++ src/api/index.js
@@ -23,12 +23,23 @@ export const fetchDailyData = async () => {
     const modifiedData = data.map((dailyData) => ({
       confirmed: dailyData.confirmed.total,
       deaths: dailyData.deaths.total,
-      date: dailyData.reportDate
-    }))
+      date: dailyData.reportDate,
+    }));
 
-    return modifiedData
+    return modifiedData;
   } catch (error) {}
 };
 
-export default fetchData;
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
 
+    return countries.map((country) => country.name)
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default fetchData;
