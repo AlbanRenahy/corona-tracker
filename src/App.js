447dc8ed1c1f7b54f7b8cb77e30eeee0ab1229f8
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-19 11:41:41 +0200

    Fetching data with axios

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from 'react';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
+import fetchData from './api'
 
 import styles from './App.module.css'
 
 
 class App extends React.Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data)
+    }
     render() {
         return (
             <div className={styles.container}>
