34cb93f79e2776d178fa0e2c61e9174c7108ee52
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-19 15:08:06 +0200

    responsive design

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
 import fetchData from "./api";
 
+import coronaImage from './images/image.jpg'
+
 import styles from "./App.module.css";
 
 class App extends React.Component {
@@ -29,6 +31,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
