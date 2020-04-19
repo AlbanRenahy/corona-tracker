8c431925dafbd79a044f5dcde263dc549a0ff27e
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-04-19 11:56:59 +0200

    cards integration with material-ui

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import Cards from './components/Cards/Cards';
-import Chart from './components/Chart/Chart';
-import CountryPicker from './components/CountryPicker/CountryPicker';
-import fetchData from './api'
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import fetchData from "./api";
 
-import styles from './App.module.css'
-
+import styles from "./App.module.css";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
 
-    async componentDidMount() {
-        const data = await fetchData();
+  async componentDidMount() {
+    const fetchedData = await fetchData();
 
-        console.log(data)
-    }
-    render() {
-        return (
-            <div className={styles.container}>
-                <Cards />
-                <CountryPicker />
-                <Chart />
-            </div>
-        )
-    }
+    this.setState({ data: fetchedData });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+
+const Cards = (props) => {
+    console.log(props);
+    
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infectés</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Nombre de cas de COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Guéris</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number de guéris du COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Décès</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Numbre de décès dûs au COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
